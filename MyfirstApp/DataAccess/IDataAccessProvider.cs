d62f044a3a0ceb8baa0c11d18c3502cb460f71fe
anchal.s@solutelabs.com <anchal.s@solutelabs.com>
2021-01-09 18:42:48 +0530

    crud-.netcore-webapi-swaggerdoc

---
+++ MyfirstApp/DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,14 @@
+﻿using MyfirstApp.Model;
+using System.Collections.Generic;
+
+namespace MyfirstApp.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
